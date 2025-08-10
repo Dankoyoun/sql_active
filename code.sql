a575e3bcba9820e78c5d7f01e504720541bcaef8
Dankoyoun <160541152+Dankoyoun@users.noreply.github.com>
2025-08-10 23:55:58 +0300

    Update code.sql

--- code.sql
+++ code.sql
@@ -45,3 +45,81 @@ SELECT
 	, 'Track unitprice' AS unitprice
 FROM Track t  
 GROUP BY 1
+
+
+-- З співбесіди !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+SELECT *
+FROM `ua-trends-468520.kaggle_data.Cats` 
+LIMIT 1000
+
+SELECT userid
+      , version
+      , COUNT(distinct version)
+FROM `ua-trends-468520.kaggle_data.Cats`
+GROUP BY 1,2 
+LIMIT 1000
+
+SELECT Distinct *
+FROM `ua-trends-468520.kaggle_data.Cats`
+LIMIT 1000
+
+userid
+
+SELECT userid 
+    , COUNT(*)
+FROM `ua-trends-468520.kaggle_data.Cats`
+GROUP BY userid, version, sum_gamerounds, retention_1, retention_7
+HAVING COUNT(*) >1
+LIMIT 1000
+
+SELECT userid
+      , retention_1
+      , retention_7
+      , CASE
+       WHEN CAST(retention_1 AS INT64) = 1 AND CAST(retention_7  AS INT64) = 1 THEN 'true'  
+       WHEN CAST(retention_1 AS INT64) = 0 AND CAST(retention_7  AS INT64) = 0 THEN 'false'
+       ELSE 'other'
+       end AS full_retention
+FROM `ua-trends-468520.kaggle_data.Cats`
+
+
+
+SELECT 
+  ROUND(SUM(CAST(retention_1 AS INT64))/1000, 2) AS total_retention_1, -- кол-во вернувшихся на 1 день
+  ROUND(SUM(CAST(retention_7 AS INT64))/1000, 2) AS total_retention_7, -- кол-во вернувшихся на 7 день
+  COUNT(*)/1000 AS total_users
+FROM `ua-trends-468520.kaggle_data.Cats`;
+
+-- Перероблено з CAST
+
+WITH retention AS (
+  SELECT 
+    version,
+    CAST(retention_1 AS INT64) AS ret_1,
+    CAST(retention_7 AS INT64) AS ret_7
+FROM `ua-trends-468520.kaggle_data.Cats`
+)
+SELECT 
+  version,
+  ROUND(SUM(ret_1) / COUNT(*) * 100, 2) AS retention_1,
+  ROUND(SUM(ret_7) / COUNT(*) * 100, 2) AS retention_7
+FROM retention
+GROUP BY version;
+
+-- Дівчина 
+
+
+WITH retention AS (
+  SELECT 
+    version,
+    IF(retention_1 = TRUE, 1, 0) AS ret_1,
+    IF(retention_7 = TRUE, 1, 0) AS ret_7
+FROM `ua-trends-468520.kaggle_data.Cats`
+)
+SELECT 
+  version,
+  ROUND(SUM(ret_1) / COUNT(ret_1) * 100, 2) AS retention_1,
+  ROUND(SUM(ret_7) / COUNT(ret_7) * 100, 2) AS retention_7
+FROM retention
+GROUP BY version;
+
