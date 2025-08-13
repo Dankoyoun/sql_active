1b40ca4102253b0c8f00f5090af59b51d86a1414
Dankoyoun <160541152+Dankoyoun@users.noreply.github.com>
2025-08-13 13:51:34 +0300

    Update prosedure.sql

--- prosedure.sql
+++ prosedure.sql
@@ -14,9 +14,9 @@ BEGIN
     VALUES (p_customerid, p_name, p_age, p_gender);
 END;
 $$;
-
+________________________________________________
 CALL customer_insert(1, 'Danya', 25, 'Male');
-
+________________________________________________
 
 2. SELECT (через функцию, т.к. процедуры не возвращают данные напрямую)
 
@@ -33,9 +33,9 @@ AS $$
     SELECT customerid, name, age, gender
     FROM customer;
 $$;
-
+________________________________________________
 SELECT * FROM customer_select_all();
-
+________________________________________________
 3. UPDATE
 
 -- Процедура для обновления данных клиента
@@ -55,9 +55,9 @@ BEGIN
     WHERE customerid = p_customerid;  -- Обновляем только одного клиента по ID
 END;
 $$;
-
+________________________________________________
 CALL customer_update(1, 'DanyaPro', 26, 'Male');
-
+________________________________________________
 4. DELETE
 
 -- Процедура для удаления клиента по ID
@@ -71,5 +71,32 @@ BEGIN
     WHERE customerid = p_customerid;  -- Удаляем только одного клиента по ID
 END;
 $$;
-
+________________________________________________
 CALL customer_delete(1);
+________________________________________________
+5. UPSERT
+    
+CREATE PROCEDURE customer_upsert(
+    p_customerid INT,
+    p_name VARCHAR(50),
+    p_age INT,
+    p_gender VARCHAR(10)
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    INSERT INTO customer (customerid, name, age, gender)
+    VALUES (p_customerid, p_name, p_age, p_gender)
+    ON CONFLICT (customerid) DO UPDATE
+    SET name = EXCLUDED.name,
+        age = EXCLUDED.age,
+        gender = EXCLUDED.gender;
+END;
+$$;
+_________________________________________________
+-- Добавление нового клиента
+CALL customer_upsert(1, 'Danya', 25, 'Male');
+
+-- Обновление клиента с тем же ID
+CALL customer_upsert(1, 'DanyaPro', 26, 'Male');
+__________________________________________________
